d30d487253db646e704a7ab2cb09f34c71084f18
jacob <jacob@kineviz.com>
2020-11-24 21:08:37 +0800

    export GNode and Edge

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 
 
 import Graph from "./graph/Graph"
+import GNode from './graph/Node'
+import Edge from './graph/Edge'
 import VF2Matcher from './matcher/VF2Matcher';
-export {Graph,VF2Matcher}
+export {Graph,VF2Matcher,GNode,Edge}
