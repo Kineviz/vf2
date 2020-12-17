7bfc90c213f29eacf6744c50ca4eefd6ec6051e9
jacob <jacob@kineviz.com>
2020-12-17 21:48:11 +0800

    add comparator

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@ import Graph from "./graph/Graph"
 import GNode from './graph/Node'
 import Edge from './graph/Edge'
 import VF2Matcher from './matcher/VF2Matcher';
-export {Graph,VF2Matcher,GNode,Edge}
+import NodeComparator from './comparator/NodeComparator'
+import EdgeComparator from './comparator/EdgeComparator'
+export {Graph,VF2Matcher,GNode,Edge,NodeComparator,EdgeComparator}
